7cdfebb48ea224fe9248d7bda0d7130372006b8c
bobstarikov <danil.starikov.1964@mail.ru>
2023-03-30 13:59:44 +0300

    New structure
    -

---
+++ Hungry-Kitties-Dapp/src/Components/NavBar/index.js
@@ -0,0 +1,6 @@
+import Information from "./Information/Information";
+import Notification from "./Notification/Notification";
+import Profile from "./Profile/Profile";
+import SideBar from "./SideBar/SideBar";
+
+export {Information, Notification, Profile, SideBar}
