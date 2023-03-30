1c85819f4a55ffbb943e7fdcdd41dbdf5a526741
bobstarikov <danil.starikov.1964@mail.ru>
2023-03-30 14:10:35 +0300

    Commit changes

--- Hungry-Kitties-Dapp/src/Components/NavBar/index.js
+++ Hungry-Kitties-Dapp/src/Components/NavBar/index.js
@@ -1,6 +1,16 @@
+<<<<<<< Updated upstream
 import Information from "./Information/Information";
 import Notification from "./Notification/Notification";
 import Profile from "./Profile/Profile";
 import SideBar from "./SideBar/SideBar";
 
 export {Information, Notification, Profile, SideBar}
+=======
+import Discover from './Discover/Discover';
+import HelpCenter from './HelpCenter/HelpCenter';
+import Profile from './Profile/Profile';
+import Notification from './Notification/Notification';
+import SideBar from './SideBar/SideBar';
+
+export {Discover, HelpCenter, Profile, Notification, SideBar}
+>>>>>>> Stashed changes
---
+++ Hungry-Kitties-Dapp/src/Components/Test.jsx
@@ -0,0 +1,17 @@
+// For Testing JSON Requests
+{/*import React from 'react';
+import Authentification from '../Back/Airtable/Authentification';
+import Faucet from '../Back/Faucet';
+import Mint from '../Back/Mint';
+
+function Test() {
+    return (
+        <div>
+            <Faucet/>
+            <Mint/>
+            <Authentification/>
+        </div>
+    );
+}
+
+export default Test;*/}
