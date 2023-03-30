588478dbaad4cd73973245c5f29d06d38368cc91
kadjit228 <kadjit228@mail.ru>
2023-03-30 22:10:54 +0300

    Deleted strange files

--- Hungry-Kitties-Dapp/src/Components/NavBar/index.js
+++
@@ -1,7 +0,0 @@
-import Information from "./Information/Information.jsx";
-import Notification from "./Notification/Notification.jsx";
-import Profile from "./Profile/Profile.jsx";
-import SideBar from "./SideBar/SideBar.jsx";
-
-export {Information, Notification, Profile, SideBar}
-
