6923c2c92823ae0b11dd3e8d23c55d79b5a2e961
kadjit228 <kadjit228@mail.ru>
2023-04-21 03:50:37 +0300

    Profile with Server

--- Hungry-Kitties-Front/hungry-kitties/src/Pages/Profile/Profile.jsx
+++ Hungry-Kitties-Front/hungry-kitties/src/Pages/Profile/Profile.jsx
@@ -1,44 +1,79 @@
 
 import '../../css/style.css'
-import avatar from  '../../img/avatar.webp'
+import avatar from '../../img/avatar.webp'
+import { ethos } from 'ethos-connect'
+import React, { useState, useEffect, Component } from 'react'
+import getUserInfo from '../../utility_functions/server/userApiRequest'
 
 const Profile = () => {
+    const { wallet } = ethos.useWallet();
+    const [user, setUser] = useState(null);
+    const [notRegister, setNotRegister] = useState(false);
 
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getUserInfo(wallet.address);
+                if (data == null) {
+                    setNotRegister(true);
+                    return;
+                }
+                setUser(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, [wallet?.address]);
+    if (!wallet) {
+        return (
+            <div>
+                <h1>Connect wallet</h1>
+            </div>
+        )
+    }
 
+    if (notRegister) {
+        return (
+            <div>
+                <h1>Not register</h1>
+            </div>
+        )
+    }
 
-    return ( 
+    return (
         <>
-        <div className="profile">
-        <div className="profile__container">
-            <div className="profile__card card__profile">
-                <div className="card__body">
-                    <img src={avatar} alt="kittik" className="card__avatar" />
-                    <div className="card__wallet-address">0x060700556ab69f9...</div>
-                    <div className="card__scan">Scan</div>
+            <div className="profile">
+                <div className="profile__container">
+                    <div className="profile__card card__profile">
+                        <div className="card__body">
+                            <img src={avatar} alt="kittik" className="card__avatar" />
+                            <div className="card__wallet-address">123</div>
+                            <div className="card__scan">Scan</div>
+                        </div>
+                    </div>
+                    <div className="profile__charity charity__profile">
+                        <div className="charity__donated">${user?.donated}</div>
+                        <div className="charity__text">You have donated to shelters!
+                        </div>
+                        <div className="charity__under-text">Thank you!</div>
+                        <div className="charity__line"></div>
+                        <div className="charity__points">272</div>
+                        <div className="charity__text">Points</div>
+                        <div className="charity__under-text">Your charity points</div>
+                        <div className="charity__line"></div>
+                        <div className="charity__discription">Charity Points are Points that affect your chance of winning a pool</div>
+                    </div>
                 </div>
-            </div>
-            <div className="profile__charity charity__profile">
-                <div className="charity__donated">$150</div>
-                <div className="charity__text">You have donated to shelters!
-                </div>
-                <div className="charity__under-text">Thank you!</div>
-                <div className="charity__line"></div>
-                <div className="charity__points">272</div>
-                <div className="charity__text">Points</div>
-                <div className="charity__under-text">Your charity points</div>
-                <div className="charity__line"></div>
-                <div className="charity__discription">Charity Points are Points that affect your chance of winning a pool</div>
-            </div>
-            </div>
 
 
-        </div>
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default Profile;
-    
+
 
 
 
--- Hungry-Kitties-Front/hungry-kitties/src/utility_functions/server/userApiRequest.js
+++ Hungry-Kitties-Front/hungry-kitties/src/utility_functions/server/userApiRequest.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const getUserInfo = async (userId) => {
   try {
-    const response = await axios.get(`/api/users/${userId}`);
+    const response = await axios.get(`http://localhost:4000/api/users/` + userId);
+    console.log(response.data);
+    if (response.code === 404) {
+      return null;
+    }
     return response.data;
   } catch (error) {
     console.error(error);
