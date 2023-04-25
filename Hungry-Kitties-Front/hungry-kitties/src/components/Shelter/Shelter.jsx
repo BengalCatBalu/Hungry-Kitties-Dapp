f77eebea91666476523389fb09c50a66c77289ca
dm1tryevs <gf334302@gmail.com>
2023-04-25 16:55:06 +0300

    added route

--- Hungry-Kitties-Front/hungry-kitties/src/components/Shelter/Shelter.jsx
+++ Hungry-Kitties-Front/hungry-kitties/src/components/Shelter/Shelter.jsx
@@ -1,5 +1,5 @@
 import shelterImage from '../../img/shelter1.webp'
-
+import { NavLink } from 'react-router-dom';
 const Shelter = () => {
     return ( 
  <>      
@@ -7,7 +7,7 @@ const Shelter = () => {
                     <img src={shelterImage} alt="shelter" className="items__img" />
                     <div className="items__footer">
                         <div className="items__text">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.</div>
-                        <div className="items__btn">Details</div>
+                        <NavLink to='/shelter'><div className="items__btn">Details</div></NavLink>
                     </div>
                 </div>
                 </>
