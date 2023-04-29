cf42b14bf46fdc7ab80f81a026de2b69a58efb86
dm1tryevs <gf334302@gmail.com>
2023-04-30 01:34:24 +0300

    downscale

--- Hungry-Kitties-Front/hungry-kitties/src/Pages/Explore/Explore.jsx
+++ Hungry-Kitties-Front/hungry-kitties/src/Pages/Explore/Explore.jsx
@@ -6,7 +6,7 @@ import Shelter from '../../components/Shelter/Shelter'
 const Explore = () => {
     return ( 
      <>
-     <div className="explore__container exlore__container-small">
+     <div className="explore__container exlore__container-smaller">
           <div className="explore__content">
                <h1 className="explore__title">Explore Charity Events On Sui</h1>
                <div className="explore__items items__explore">
--- Hungry-Kitties-Front/hungry-kitties/src/utils/slider/Slider.jsx
+++ Hungry-Kitties-Front/hungry-kitties/src/utils/slider/Slider.jsx
@@ -1,12 +1,12 @@
 import img1 from '../../img/shelter1.webp'
 import img2 from '../../img/shelter2.jpg'
 import img3 from '../../img/shelter3.webp'
-import { Navigation} from 'swiper';
+import { Navigation, Autoplay} from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import './slider.css'
+import './slider.css' 
 import 'swiper/css';
 import 'swiper/css/navigation';
 
@@ -15,10 +15,11 @@ export default () => {
     
     <Swiper
       // install Swiper modules
-      modules={[Navigation]}
+      modules={[Navigation, Autoplay]}
       spaceBetween={50}
       slidesPerView={1}
       navigation
+      autoplay
     >
       <SwiperSlide className='slide'><img src={img1} alt=""/></SwiperSlide>
       <SwiperSlide><img src={img2} alt=""/></SwiperSlide>
